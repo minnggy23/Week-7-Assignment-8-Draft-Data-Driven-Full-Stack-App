42e0dafd4ae03c46e93d1c3c875d001bbfd42f44
minnggy23 <rubyshemaria@gmail.com>
2024-10-09 14:39:30 -0700

    3rd working

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -34,83 +34,25 @@ const updateWithRating = async (
 	newRatingDocument,
 	review
 ) => {
-	const restaurant = await transaction.get(docRef);
-	const data = restaurant.data();
-	const newNumRatings = data?.numRatings ? data.numRatings + 1 : 1;
-	const newSumRating = (data?.sumRating || 0) + Number(review.rating);
-	const newAverage = newSumRating / newNumRatings;
-
-	transaction.update(docRef, {
-			numRatings: newNumRatings,
-			sumRating: newSumRating,
-			avgRating: newAverage,
-	});
-
-	transaction.set(newRatingDocument, {
-			...review,
-			timestamp: Timestamp.fromDate(new Date()),
-	});
+	return;
 };
+
+export async function addReviewToRestaurant(db, restaurantId, review) {
+	return;
+}
+
 function applyQueryFilters(q, { category, city, price, sort }) {
-	if (category) {
-			q = query(q, where("category", "==", category));
-	}
-	if (city) {
-			q = query(q, where("city", "==", city));
-	}
-	if (price) {
-			q = query(q, where("price", "==", price.length));
-	}
-	if (sort === "Rating" || !sort) {
-			q = query(q, orderBy("avgRating", "desc"));
-	} else if (sort === "Review") {
-			q = query(q, orderBy("numRatings", "desc"));
-	}
-	return q;
+	return;
 }
 
-export async function getRestaurants(db = db, filters = {}) {
-	let q = query(collection(db, "restaurants"));
-
-	q = applyQueryFilters(q, filters);
-	const results = await getDocs(q);
-	return results.docs.map(doc => {
-			return {
-					id: doc.id,
-					...doc.data(),
-					// Only plain objects can be passed to Client Components from Server Components
-					timestamp: doc.data().timestamp.toDate(),
-			};
-	});
-}
 export async function getRestaurants(db = db, filters = {}) {
 	return [];
 }
 
 export function getRestaurantsSnapshot(cb, filters = {}) {
-	if (typeof cb !== "function") {
-			console.log("Error: The callback parameter is not a function");
-			return;
-	}
-
-	let q = query(collection(db, "restaurants"));
-	q = applyQueryFilters(q, filters);
-
-	const unsubscribe = onSnapshot(q, querySnapshot => {
-			const results = querySnapshot.docs.map(doc => {
-					return {
-							id: doc.id,
-							...doc.data(),
-							// Only plain objects can be passed to Client Components from Server Components
-							timestamp: doc.data().timestamp.toDate(),
-					};
-			});
-
-			cb(results);
-	});
-
-	return unsubscribe;
+	return;
 }
+
 export async function getRestaurantById(db, restaurantId) {
 	if (!restaurantId) {
 		console.log("Error: Invalid ID received: ", restaurantId);
@@ -174,30 +116,24 @@ export function getReviewsSnapshotByRestaurantId(restaurantId, cb) {
 	return unsubscribe;
 }
 
-export async function addReviewToRestaurant(db, restaurantId, review) {
-	if (!restaurantId) {
-			throw new Error("No restaurant ID has been provided.");
-	}
-
-	if (!review) {
-			throw new Error("A valid review has not been provided.");
-	}
-
-	try {
-			const docRef = doc(collection(db, "restaurants"), restaurantId);
-			const newRatingDocument = doc(
-					collection(db, `restaurants/${restaurantId}/ratings`)
+export async function addFakeRestaurantsAndReviews() {
+	const data = await generateFakeRestaurantsAndReviews();
+	for (const { restaurantData, ratingsData } of data) {
+		try {
+			const docRef = await addDoc(
+				collection(db, "restaurants"),
+				restaurantData
 			);
 
-			// corrected line
-			await runTransaction(db, transaction =>
-					updateWithRating(transaction, docRef, newRatingDocument, review)
-			);
-	} catch (error) {
-			console.error(
-					"There was an error adding the rating to the restaurant",
-					error
-			);
-			throw error;
+			for (const ratingData of ratingsData) {
+				await addDoc(
+					collection(db, "restaurants", docRef.id, "ratings"),
+					ratingData
+				);
+			}
+		} catch (e) {
+			console.log("There was an error adding the document");
+			console.error("Error adding document: ", e);
+		}
 	}
 }
